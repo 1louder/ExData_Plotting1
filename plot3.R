66464adc03d97362f65dc8b5b1a4e9da102f82ec
onelouder <eis99@hotmail.com>
2014-05-09 14:48:58 -0500

    message

--- plot3.R
+++ plot3.R
@@ -10,12 +10,11 @@ plot3b <- as.numeric(subDates$Sub_metering_2)
 plot3c <- as.numeric(subDates$Sub_metering_3)
 days <- strptime(paste(subDates$Date, subDates$Time), format="%d/%m/%Y %H:%M:%S")
 
+## create a .png file of the plot
+png("plot3.png", width = 480, height = 480, units = "px")
 plot(days, plot3a, type="n", xlab="", ylab="Energy sub metering")
 points(days, plot3a, type="l", col="black")
 points(days, plot3b, type="l", col="red")
 points(days, plot3c, type="l", col="blue")
 legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-
-## create a .png file of the plot
-dev.copy(png, file = "plot3.png", width = 480, height = 480, units = "px")
 dev.off()
