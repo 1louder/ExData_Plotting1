50bc9e92a27e7b507238896f00ca644a8b7d0cd8
onelouder <eis99@hotmail.com>
2014-05-09 14:07:38 -0500

    message

--- plot3.R
+++ plot3.R
@@ -14,7 +14,7 @@ plot(days, plot3a, type="n", xlab="", ylab="Energy sub metering")
 points(days, plot3a, type="l", col="black")
 points(days, plot3b, type="l", col="red")
 points(days, plot3c, type="l", col="blue")
-legend("topright", pch="l", col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 ## create a .png file of the plot
 dev.copy(png, file = "plot3.png", width = 480, height = 480, units = "px")
