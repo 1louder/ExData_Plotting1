8fa864e0fee72774d8b8c2d897eb3a1c5c3e963f
1louder <1louder@users.noreply.github.com>
2014-05-09 09:36:50 -0500

    Update and rename plot1 to plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,12 @@
+## read electric power dataset into R
+
 data <- read.table("household_power_consumption.txt", sep=";", as.is=TRUE, header=TRUE)
+
+## focus on data from February 1-2, 2007
+
 subDates <- subset(data, data$Date == "1/2/2007" | data$Date == "2/2/2007")
+
+## create a histogram of Global Active Power with red bars
+
 plot1 <- as.numeric(subDates$Global_active_power)
 hist(plot1, main="Global Active Power", col="red", xlab="Global Active Power (kilowatts)", ylab = "Frequency")
