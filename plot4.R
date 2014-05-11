2d17e8bda2a8a601c89b23f61691b726fe10269a
onelouder <eis99@hotmail.com>
2014-05-10 21:48:13 -0500

    message

--- plot4.R
+++ plot4.R
@@ -27,7 +27,7 @@ plot(days, plot3a, type="n", xlab="", ylab="Energy sub metering")
 points(days, plot3a, type="l", col="black")
 points(days, plot3b, type="l", col="red")
 points(days, plot3c, type="l", col="blue")
-legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lty=1, bty="n", col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 ## lower right plot
 plot4 <- as.numeric(subDates$Global_reactive_power)
